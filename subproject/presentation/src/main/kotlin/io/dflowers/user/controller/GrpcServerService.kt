99ddd718b4dfb10daa49766503b7bfdbe1938367
mango <mchead1@naver.com>
2024-12-01 22:32:27 +0900

    feat: implemented return a response data in grpc service layer

--- subproject/presentation/src/main/kotlin/io/dflowers/user/controller/GrpcServerService.kt
+++ subproject/presentation/src/main/kotlin/io/dflowers/user/controller/GrpcServerService.kt
@@ -1,13 +1,21 @@
 package io.dflowers.user.controller
 
+import com.google.protobuf.Timestamp
 import io.dflowers.user.grpc.GetUserByIdRequest
 import io.dflowers.user.grpc.GetUserByIdResponse
 import io.dflowers.user.grpc.UserServiceGrpcKt
 import net.devh.boot.grpc.server.service.GrpcService
+import java.time.Instant
+import java.util.*
 
 @GrpcService
 class GrpcServerService : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
     override suspend fun getUserById(request: GetUserByIdRequest): GetUserByIdResponse {
-        return super.getUserById(request)
+        return GetUserByIdResponse.newBuilder()
+            .setAge(5)
+            .setName("test")
+            .setId(request.id!!)
+            .setCreated(Timestamp.newBuilder())
+            .build()
     }
 }
