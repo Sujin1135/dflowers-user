d7daa2e4e551a94fa1545ebbdba6991923c75203
mango <mchead1@naver.com>
2024-12-02 15:41:33 +0900

    chore: set module implementations by each layered architecture module

--- subproject/presentation/build.gradle.kts
+++ subproject/presentation/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation(project(":subproject:interface"))
+    implementation(project(":subproject:application"))
+    implementation(project(":subproject:domain"))
 
     implementation(libs.grpc.spring)
     implementation(libs.grpc.services)
