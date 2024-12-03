1d55240088902cba33ccace3cbd3be0a3446f4f8
mango <mchead1@naver.com>
2024-12-03 12:56:15 +0900

    feat: configured flyway migration

--- subproject/infrastructure/build.gradle.kts
+++ subproject/infrastructure/build.gradle.kts
@@ -1,9 +1,44 @@
 plugins {
+    alias(libs.plugins.flyway)
     alias(libs.plugins.jooq)
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.postgresql)
+        classpath(libs.flyway.postgresql)
+        classpath(libs.jooq.codegen)
+    }
 }
 
 dependencies {
     implementation(project(":subproject:domain"))
 
     implementation(libs.bundles.db)
+
+    implementation(libs.bundles.flyway)
+}
+
+val dbHost: String = System.getenv("DB_HOST") ?: "127.0.0.1"
+val dbPort: String = System.getenv("DB_PORT") ?: "5432"
+val dbSchema: String = System.getenv("DB_SCHEMA") ?: "user"
+val dbUser: String = System.getenv("DB_USER") ?: "postgres"
+val dbPassword: String = System.getenv("DB_PASSWORD") ?: "password123!"
+
+flyway {
+    driver = "org.postgresql.Driver"
+    url = "jdbc:postgresql://$dbHost:$dbPort/:$dbSchema"
+    baselineVersion = "1"
+    baselineDescription = "true"
+    table = "migration_history"
+    user = dbUser
+    password = dbPassword
+    connectRetries = 60
+    locations =
+        listOf("filesystem:src/main/resources/db/migration/")
+            .toTypedArray()
+    cleanDisabled = false
 }
---
+++ subproject/infrastructure/src/main/resources/db/migration/V1_1__create_tables.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table if not exists users
+(
+    id       uuid                                                            not null
+        primary key,
+    email    varchar(355)                                                    not null,
+    password varchar(255)                                                    not null,
+    name     varchar(50)                                                     not null,
+    deleted  timestamp                                                       null,
+    created  timestamp default CURRENT_TIMESTAMP                             not null,
+    modified timestamp default CURRENT_TIMESTAMP                             not null
+);
