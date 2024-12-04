91cb0426958ca4e07b177c03d6f590800daeeae2
mango <mchead1@naver.com>
2024-12-04 19:29:30 +0900

    feat: added the table of owner user

--- subproject/infrastructure/src/main/resources/db/migration/V1_1__create_tables.sql
+++ subproject/infrastructure/src/main/resources/db/migration/V1_1__create_tables.sql
@@ -11,3 +11,15 @@ create table if not exists users
     created  timestamp default CURRENT_TIMESTAMP                             not null,
     modified timestamp default CURRENT_TIMESTAMP                             not null
 );
+
+create table if not exists owners
+(
+    id       uuid                                                            not null
+    primary key,
+    email    varchar(355)                                                    not null,
+    password varchar(255)                                                    not null,
+    name     varchar(50)                                                     not null,
+    deleted  timestamp                                                       null,
+    created  timestamp default CURRENT_TIMESTAMP                             not null,
+    modified timestamp default CURRENT_TIMESTAMP                             not null
+);
