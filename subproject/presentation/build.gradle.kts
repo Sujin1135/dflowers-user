26b61aa789cd90908b86789f1277e381f94c61e1
mango <mchead1@naver.com>
2025-01-10 19:25:19 +0900

    feat: added the feature to login of mutation

--- subproject/presentation/build.gradle.kts
+++ subproject/presentation/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    alias(libs.plugins.dgs.codegen)
+}
+
 dependencies {
     implementation(project(":subproject:interface"))
     implementation(project(":subproject:application"))
@@ -9,4 +13,10 @@ dependencies {
     implementation(libs.swagger)
     implementation(libs.spring.validation)
     implementation(libs.bundles.graphql)
+}
+
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+    schemaPaths = listOf("$projectDir/src/main/resources/schema").toMutableList() // 스키마 경로
+    packageName = "io.dflowers.user.graphql" // 생성될 Java/Kotlin 클래스 패키지 경로
+    generateClient = true // 타입 안전한 쿼리 API 생성 활성화
 }
--- subproject/presentation/src/main/kotlin/io/dflowers/user/graphql/datafetcher/UserDataFetcher.kt
+++ subproject/presentation/src/main/kotlin/io/dflowers/user/graphql/datafetcher/UserDataFetcher.kt
@@ -1,12 +1,56 @@
 package io.dflowers.user.graphql.datafetcher
 
+import arrow.core.raise.get
+import arrow.core.raise.mapError
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
+import com.netflix.graphql.dgs.InputArgument
+import io.dflowers.user.dto.ErrorCode
+import io.dflowers.user.dto.SignInRequest
 import io.dflowers.user.entity.User
+import io.dflowers.user.exception.HttpException
+import io.dflowers.user.graphql.types.SignInResponse
+import io.dflowers.user.graphql.types.TokenInfo
+import io.dflowers.user.service.FindOneUser
+import io.dflowers.user.util.JwtUtil
+import kotlinx.coroutines.reactor.mono
+import org.springframework.security.crypto.password.PasswordEncoder
+import reactor.core.publisher.Mono
 
 @DgsComponent
-class UserDataFetcher {
-    private val testFixtures =
-        listOf(
-            User,
-        )
+class UserDataFetcher(
+    private val findOneUser: FindOneUser,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtUtil: JwtUtil,
+) {
+    @DgsMutation
+    fun signIn(
+        @InputArgument input: SignInRequest,
+    ): Mono<SignInResponse> =
+        mono {
+            val user =
+                findOneUser(User.Email(input.email))
+                    .mapError {
+                        when (it) {
+                            FindOneUser.Failure.NotFoundUser -> throw HttpException.NotFound.create(
+                                objectName = User::class.simpleName!!,
+                                objectId = input.email,
+                            )
+                        }
+                    }.get()
+
+            if (!passwordEncoder.matches(input.password, user.password.value)) {
+                throw HttpException.Unauthorized.create(ErrorCode.WRONG_CREDENTIALS, "사용자 이메일 혹은 비밀번호가 올바르지 않습니다.")
+            }
+            val token = jwtUtil.createAuthTokens(input.email)
+            SignInResponse(
+                tokenInfo =
+                    TokenInfo(
+                        accessToken = token.accessToken,
+                        accessTokenExpiresIn = token.accessTokenExpiresIn,
+                        refreshToken = token.refreshToken,
+                        refreshTokenExpiresIn = token.refreshTokenExpiresIn,
+                    ),
+            )
+        }
 }
