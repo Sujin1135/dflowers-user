f95a9e1b9cdd56f65c0dc0d965fe8931daafe6e9
mango <mchead1@naver.com>
2025-02-07 02:55:59 +0900

    WIP: leave the todo list

---
+++ subproject/application/src/test/kotlin/io/dflowers/user/service/SignInWithOauth2Test.kt
@@ -0,0 +1,4 @@
+package io.dflowers.user.service
+
+// TODO: It'll be wrote
+class SignInWithOauth2Test
---
+++ subproject/application/src/test/kotlin/io/dflowers/user/service/SignUpWithOauth2Test.kt
@@ -0,0 +1,4 @@
+package io.dflowers.user.service
+
+// TODO: It'll be wrote
+class SignUpWithOauth2Test
