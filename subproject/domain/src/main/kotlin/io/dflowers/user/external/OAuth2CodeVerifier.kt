96c122ad8a875c50bd6b0dfc8ab49e8c8dbadc7f
mango <mchead1@naver.com>
2025-02-07 16:29:28 +0900

    docs: leave todo list in OAuth2CodeVerifier

--- subproject/domain/src/main/kotlin/io/dflowers/user/external/OAuth2CodeVerifier.kt
+++ subproject/domain/src/main/kotlin/io/dflowers/user/external/OAuth2CodeVerifier.kt
@@ -4,6 +4,7 @@ import arrow.core.raise.Effect
 import io.dflowers.user.entity.OAuth2TokenResponse
 import io.dflowers.user.entity.OAuth2UserInfo
 
+// TODO: define left values
 interface OAuth2CodeVerifier {
     fun verifyForSignIn(code: String): Effect<Nothing, OAuth2TokenResponse>
 
